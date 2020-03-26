201161ee18a0ec765853bc04d2c63c97b34e2909
Pascal Rainville <pascal.rainville@gmail.com>
2020-03-26 08:13:56 -0400

    ...

--- programme/programme.ino
+++ programme/programme.ino
@@ -1,3 +1,39 @@
+#include <GSM3VoiceCallService.h>
+#include <GSM3ShieldV1ModemCore.h>
+#include <GSM3MobileClientService.h>
+#include <GSM3CircularBuffer.h>
+#include <GSM3MobileSMSProvider.h>
+#include <GSM3ShieldV1DataNetworkProvider.h>
+#include <GSM3MobileDataNetworkProvider.h>
+#include <GSM3MobileMockupProvider.h>
+#include <GSM3MobileAccessProvider.h>
+#include <GSM3MobileServerService.h>
+#include <GSM3ShieldV1BaseProvider.h>
+#include <GSM3ShieldV1CellManagement.h>
+#include <GSM3ShieldV1ModemVerification.h>
+#include <GSM3ShieldV1MultiServerProvider.h>
+#include <GSM3MobileNetworkRegistry.h>
+#include <GSM3ShieldV1SMSProvider.h>
+#include <GSM3ShieldV1BandManagement.h>
+#include <GSM3ShieldV1.h>
+#include <GSM3ShieldV1ServerProvider.h>
+#include <GSM3ShieldV1VoiceProvider.h>
+#include <GSM3SMSService.h>
+#include <GSM3MobileServerProvider.h>
+#include <GSM.h>
+#include <GSM3ShieldV1ScanNetworks.h>
+#include <GSM3ShieldV1ClientProvider.h>
+#include <GSM3MobileClientProvider.h>
+#include <GSM3MobileCellManagement.h>
+#include <GSM3MobileNetworkProvider.h>
+#include <GSM3ShieldV1MultiClientProvider.h>
+#include <GSM3SoftSerial.h>
+#include <GSM3ShieldV1DirectModemProvider.h>
+#include <GSM3IO.h>
+#include <GSM3MobileVoiceProvider.h>
+#include <GSM3ShieldV1PinManagement.h>
+#include <GSM3ShieldV1AccessProvider.h>
+
 // Programme pour ma machine a cuisiner!
 // Version 2, controler par mon application web!
 
@@ -157,11 +193,22 @@ void loop() {
 }
 
 void printDebugInfo() {
-  Serial.println("* In *");
+  Serial.println("***");
   Serial.print("Axis: ");
   Serial.println(selectedAxis);
   Serial.print("Speed: ");
   Serial.println(selectedSpeed);
+  Serial.print("PositionX: ");
+  Serial.println(positionX);
+  Serial.print("Is manual mode: ");
+  Serial.println(isManualMode);
+  Serial.print("isReferenced: ");
+  Serial.println(isReferenced);
+  Serial.print("isReferencing: ");
+  Serial.println(isReferencing);
+  Serial.println("* In *");
+  Serial.print("Limit switch pin: ");
+  Serial.println(digitalRead(limitSwitchPin));
   Serial.println("* Out *");
   Serial.print("Enabled: ");
   Serial.println(digitalRead(motorEnabledPin));
