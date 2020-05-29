9e5e0af5792bb038a960cc37ace6d28133b2b292
Dark Krael <dark.krael@gmail.com>
2020-05-29 18:10:56 -0400

    ...

--- redirect_usb.sh
+++ redirect_usb.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 sudo cat /dev/ttyUSB0 | nc 192.168.0.18 1234
 
+# sudo usermod -a -G dialout pascalr.
 # utils:socat /dev/ttyUSB0,raw,echo=0 tcp-listen:8888,reuseaddr
 # usb-devices
 # dmesg
 
+socat /dev/ttyUSB0,raw,echo=0,b115200 tcp-listen:8888,reuseaddr
 socat /dev/ttyUSB0,raw,echo=0 tcp-listen:8888,reuseaddr
 
 # Then on 1st PC you can connect to 2nd PC with socat and provide the data on a pseudo terminal /dev/ttyVUSB0 for your application:
 
 #socat PTY,raw,echo=0,link=/dev/ttyVUSB0 tcp:192.168.0.18:8888
-socat PTY,raw,echo=0,link=/dev/ttyUSB0 tcp:192.168.0.18:8888
+socat PTY,raw,echo=0,link=/dev/ttyUSB0,b115200 tcp:192.168.0.18:8888
 
 
 
