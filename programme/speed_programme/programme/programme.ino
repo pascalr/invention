86cd8c10b3e0b53c2b9713cefca297bd9bd87eaf
Dark Krael <dark.krael@gmail.com>
2020-06-02 10:26:07 -0400

    ...

--- programme/speed_programme/programme/programme.ino
+++ programme/speed_programme/programme/programme.ino
@@ -1,39 +1,3 @@
-#include <GSM3VoiceCallService.h>
-#include <GSM3ShieldV1ModemCore.h>
-#include <GSM3MobileClientService.h>
-#include <GSM3CircularBuffer.h>
-#include <GSM3MobileSMSProvider.h>
-#include <GSM3ShieldV1DataNetworkProvider.h>
-#include <GSM3MobileDataNetworkProvider.h>
-#include <GSM3MobileMockupProvider.h>
-#include <GSM3MobileAccessProvider.h>
-#include <GSM3MobileServerService.h>
-#include <GSM3ShieldV1BaseProvider.h>
-#include <GSM3ShieldV1CellManagement.h>
-#include <GSM3ShieldV1ModemVerification.h>
-#include <GSM3ShieldV1MultiServerProvider.h>
-#include <GSM3MobileNetworkRegistry.h>
-#include <GSM3ShieldV1SMSProvider.h>
-#include <GSM3ShieldV1BandManagement.h>
-#include <GSM3ShieldV1.h>
-#include <GSM3ShieldV1ServerProvider.h>
-#include <GSM3ShieldV1VoiceProvider.h>
-#include <GSM3SMSService.h>
-#include <GSM3MobileServerProvider.h>
-#include <GSM.h>
-#include <GSM3ShieldV1ScanNetworks.h>
-#include <GSM3ShieldV1ClientProvider.h>
-#include <GSM3MobileClientProvider.h>
-#include <GSM3MobileCellManagement.h>
-#include <GSM3MobileNetworkProvider.h>
-#include <GSM3ShieldV1MultiClientProvider.h>
-#include <GSM3SoftSerial.h>
-#include <GSM3ShieldV1DirectModemProvider.h>
-#include <GSM3IO.h>
-#include <GSM3MobileVoiceProvider.h>
-#include <GSM3ShieldV1PinManagement.h>
-#include <GSM3ShieldV1AccessProvider.h>
-
 // OUTPUT PINS
 #define ledPin 13
 
@@ -49,6 +13,9 @@
 
 class Axis {
   public:
+    Axis() {
+      
+    }
     unsigned long position;
     unsigned long destination;
     unsigned long maxPosition;
@@ -76,10 +43,10 @@ class Axis {
 
 // The horizontal axis adjusts it's speed to compensate the rotary axis
 class HorizontalAxis: public Axis {
-  private:
-  Axis& m_rotary_axis;
-  
   public:
+    HorizontalAxis() : Axis {} {
+      
+    }
   
   unsigned int getDelay() {
     // TODO: The speed is not fixed.
