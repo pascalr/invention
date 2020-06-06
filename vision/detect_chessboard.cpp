8ca8b5f598a306d56aeece7c9799139b5073dc36
Dark Krael <dark.krael@gmail.com>
2020-06-06 17:30:25 -0400

    ...

--- vision/detect_chessboard.cpp
+++ vision/detect_chessboard.cpp
@@ -10,6 +10,10 @@
 // The rotating axis is referenced when it is farther than the middle.
 // The vertical axis is referenced when it it higher than the middle (put another 8 by 8 grid on the other side would be the simplest)
 // The horizontal axis is referenced when the squares are bigger than some threshold, offset for zero.
+//
+// TODO: Print with white around, see note below.
+// Note
+//    The function requires white space (like a square-thick border, the wider the better) around the board to make the detection more robust in various environments. Otherwise, if there is no border and the background is dark, the outer black squares cannot be segmented properly and so the square grouping and ordering algorithm fails.
 
 using namespace cv;
 using namespace std;
@@ -39,6 +43,7 @@ int main(int, char**)
     //--- GRAB AND WRITE LOOP
     cout << "Start grabbing" << endl
         << "Press any key to terminate" << endl;
+    //for (int once = 0; once <= 1; once++)
     for (;;)
     {
         // wait for a new frame from camera and store it into 'frame'
@@ -49,21 +54,33 @@ int main(int, char**)
             break;
         }
 
+	rotate(frame, frame, ROTATE_180);
         cvtColor(frame, gray, COLOR_BGR2GRAY);
         bool patternfound = findChessboardCorners(gray, patternsize, corners,
           CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + CALIB_CB_FAST_CHECK);
+        //bool patternfound = findChessboardCornersSB(gray, patternsize, corners,
+         // CALIB_CB_NORMALIZE_IMAGE);
 
         if(patternfound) {
-          cornerSubPix(gray, corners, Size(11, 11), Size(-1, -1), TermCriteria(2, 30, 0.1));
+		// The following is not needed with the SB function
+          //cornerSubPix(gray, corners, Size(11, 11), Size(-1, -1), TermCriteria(2, 30, 0.1));
           //cornerSubPix(gray, corners, Size(11, 11), Size(-1, -1),
            // TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+	   //
+
+	  Point2f center = corners[24];
+
+	  if (center.x > frame.cols/2) {
+            // The reference is done, the rotating axis is at 0 degrees or less.
+            circle(frame, center, 10, Scalar(0, 0, 255), FILLED);
+	  }
          
-          drawChessboardCorners(frame, patternsize, Mat(corners), patternfound);
+          //drawChessboardCorners(frame, patternsize, Mat(corners), patternfound);
 	}
 
         // show live and wait for a key with timeout long enough to show images
         imshow("Live", frame);
-        if (waitKey(5) >= 0)
+        if (waitKey(40) >= 0)
             break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
