9c7ec22c837bafc07d9f40f5100a393194d43ea5
Dark Krael <dark.krael@gmail.com>
2020-10-11 10:54:34 -0400

    .

--- src/video.cpp
+++ src/video.cpp
@@ -19,7 +19,20 @@ int main(int, char**)
     cerr << "ERROR! Unable to open camera\n";
     return -1;
   }
+  // https://www.arducam.com/product/b0196arducam-8mp-1080p-usb-camera-module-1-4-cmos-imx219-mini-uvc-usb2-0-webcam-board-with-1-64ft-0-5m-usb-cable-for-windows-linux-android-and-mac-os/
+  // Adjustable Features: Brightness, Contrast, Saturation, Sharpness, Gamma, White balance.
+  //And a lot more... https://docs.opencv.org/3.4/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
   cout << "Camera opened ("<< cap.get(CAP_PROP_FRAME_WIDTH) << "x" << cap.get(CAP_PROP_FRAME_HEIGHT) << ")\n";
+  cout << "Brightness: " << cap.get(CAP_PROP_BRIGHTNESS) << endl;
+  cout << "Contrast: " << cap.get(CAP_PROP_CONTRAST) << endl;
+  cout << "Saturation: " << cap.get(CAP_PROP_SATURATION) << endl;
+  cout << "Sharpness: " << cap.get(CAP_PROP_SHARPNESS) << endl;
+  cout << "Gamma: " << cap.get(CAP_PROP_GAMMA) << endl;
+  cout << "Exposure: " << cap.get(CAP_PROP_EXPOSURE) << endl;
+  cout << "Auto exposure: " << cap.get(CAP_PROP_AUTO_EXPOSURE) << endl;
+  // White balance???
+  cout << "Hue: " << cap.get(CAP_PROP_HUE) << endl;
+  cout << "Gain: " << cap.get(CAP_PROP_GAIN) << endl;
   for (;;) {
     cap.read(frame);
     if (frame.empty()) {
